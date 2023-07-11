0ecba782bcffe264ab4c7dad6dfbada85f0dd5b0
chuantt <chuan123789@gmail.com>
2023-07-11 15:07:47 +0700

    update plugin api

--- src/components/PluginChat/index.tsx
+++ src/components/PluginChat/index.tsx
@@ -1,32 +1,40 @@
 "use client";
+import { useSettings } from "@/layout/ProviderSettings";
 import { useEffect } from "react";
 
 const PluginChat = () => {
+  const { data } = useSettings();
+
   useEffect(() => {
-    const chatbox = document.getElementById("fb-customer-chat");
-    if (chatbox) {
-      chatbox.setAttribute("page_id", "116506126456707");
-      chatbox.setAttribute("attribution", "biz_inbox");
+    const fanpage_id = data?.company?.fanpage_id;
 
-      window.fbAsyncInit = function () {
-        // eslint-disable-next-line no-undef
-        window.FB.init({
-          xfbml: true,
-          version: "v17.0",
-        });
-      };
+    if (fanpage_id) {
+      const chatbox = document.getElementById("fb-customer-chat");
+      if (chatbox) {
+        chatbox.setAttribute("page_id", fanpage_id);
+        chatbox.setAttribute("attribution", "biz_inbox");
 
-      (function (d, s, id) {
-        let js,
-          fjs = d.getElementsByTagName(s)[0] as HTMLScriptElement;
-        if (d.getElementById(id)) return;
-        js = d.createElement(s) as HTMLScriptElement;
-        js.id = id;
-        js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
-        fjs && fjs?.parentNode?.insertBefore(js, fjs);
-      })(document, "script", "facebook-jssdk");
+        window.fbAsyncInit = function () {
+          // eslint-disable-next-line no-undef
+          window.FB.init({
+            xfbml: true,
+            version: "v17.0",
+          });
+        };
+
+        (function (d, s, id) {
+          let js,
+            fjs = d.getElementsByTagName(s)[0] as HTMLScriptElement;
+          if (d.getElementById(id)) return;
+          js = d.createElement(s) as HTMLScriptElement;
+          js.id = id;
+          js.src =
+            "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+          fjs && fjs?.parentNode?.insertBefore(js, fjs);
+        })(document, "script", "facebook-jssdk");
+      }
     }
-  }, []);
+  }, [data?.company?.fanpage_id]);
 
   return (
     <>
